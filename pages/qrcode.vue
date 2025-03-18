7a40ee593b8ec193f33f0368f0dee6342f2fd378
liujunxiang0076 <liujunxiang0076@gmail.com>
2025-03-18 17:15:30 +0800

    fix: 优化二维码生成器页面图标配置
    - 移除全局图标配置
    - 添加页面级别的图标设置
    - 修复页面标题和图标导入问题

--- pages/qrcode.vue
+++ pages/qrcode.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <QRCodeGenerator />
-  </div>
+  <QRCodeGenerator />
 </template>
 
 <script setup>
-import QRCodeGenerator from '~/components/QRCodeGenerator.vue'
+import { useHead } from '#imports'
+
+useHead({
+  title: '二维码生成器',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PHBhdGggZmlsbD0iIzQwOWVmZiIgZD0iTTMgM2g3djdIM3ptMiAydjNoM1Y1em0tMiA3aDd2N0gzem0yIDJ2M2gzdi0zem03LTlIMjJ2N2gtN3ptMiAydjNoM1Y1ek0zIDN2N2g3VjN6bTkgMHY3aDdWM3ptLTcgOXY3aDdWMTJ6Ii8+PC9zdmc+'
+    }
+  ]
+})
 </script> 
